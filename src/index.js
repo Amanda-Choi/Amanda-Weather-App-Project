1473be62616f09420fe86612056a3b0296fdef49
Amanda-Choi <96817093+Amanda-Choi@users.noreply.github.com>
2021-12-30 18:11:07 -0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,90 @@
+// show current Date & Time //
+let now = new Date();
+let h2 = document.querySelector("h2");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+h2.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes}`;
+
+/////////////////F/C conversion////////////////
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `28°`;
+}
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `-5°`;
+}
+
+/////////////////////show search city input and temp//////////////////////
+
+function showTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#max").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#min").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+}
+
+function search(city) {
+  let apiKey = "9eb999725a805b7110429e48012734fb";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#form").value;
+  search(city);
+}
+let form = document.querySelector("#search-bar");
+form.addEventListener("submit", handleSubmit);
+
+search("Seattle");
